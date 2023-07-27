a835e7b45a2cffd65cedb9bc1f17a6a616645d28
Franklin Shack <franklinshack@gmail.com>
2023-07-27 10:38:12 -0400

    fixed navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,25 +3,24 @@ import styles from '../page.module.css'; // Adjust the path to your CSS file
 
 import ExternalLink from 'public/external-link.svg';
 import LinkedIn from 'public/linkedin.svg';
+import Mail from 'public/email.svg';
 
 const Navbar = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.gridcontainer}>
-        <div className={styles.navbar}>
-          <a className={styles.griditem} href="https://www.linkedin.com/in/fshack/">
-            LinkedIn
-            <LinkedIn />
-            </a>
-          <a className={styles.griditem} href="https://github.com/FranklinShack">
-            GitHub
-            <ExternalLink />
-            </a>
-          <a className={styles.griditem} href="mailto:franklin@franklinshack.com">
-            Contact
-          
+    <main className={styles.gridcontainer}>
+      <div className={styles.navbar}>
+        <a className={styles.griditem} href="https://www.linkedin.com/in/fshack/">
+          LinkedIn 
+          <LinkedIn className={styles.navicon} />
           </a>
-        </div>
+        <a className={styles.griditem} href="https://github.com/FranklinShack">
+          GitHub 
+          <ExternalLink className={styles.navicon} />
+          </a>
+        <a className={styles.griditem} href="mailto:franklin@franklinshack.com">
+          Contact 
+          <Mail className={styles.navicon} />
+        </a>
       </div>
     </main>
   );
